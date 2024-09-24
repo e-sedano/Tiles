8f9768eb5131ad20ec7a450f7eec294b2d843bd5
Daniel <155811921+dnlaglr@users.noreply.github.com>
2024-09-23 18:08:34 -0700

    Setup BrowserRouter

--- tiles/src/main.tsx
+++ tiles/src/main.tsx
@@ -1,7 +1,31 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { Outlet, RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Navbar from './components/Navbar'
+import Tiles from './views/tiles'
+
+function Layout() {
+  return (
+    <>
+      <Navbar />
+      <Outlet />
+    </>
+  )
+}
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '',
+        element: <Tiles />
+      }
+    ]
+  }
+])
 
 createRoot(document.getElementById('root')!).render(
-  <>
-  </>,
+  <RouterProvider router={router} />
 )
