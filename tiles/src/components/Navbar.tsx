d6976ed71a86815c0f085ab2cfc2627b50a52107
Daniel <155811921+dnlaglr@users.noreply.github.com>
2024-09-23 18:08:48 -0700

    Create basic Navbar

---
+++ tiles/src/components/Navbar.tsx
@@ -0,0 +1,30 @@
+import { Link } from 'react-router-dom';
+
+const navLinks = [
+  {
+    path: '/map',
+    name: 'Map'
+  },
+]
+
+export default function Navbar() {
+  return (
+    <div className='flex justify-center items-center w-screen h-14 px-64'>
+      <div className='flex flex-row justify-between items-center w-full h-full'>
+        <Link to='/'>
+          <h1 className='text-gray-900 text-2xl font-semibold'>Tiles</h1>
+        </Link>
+        <ul>
+          {navLinks.map((link, index) => (
+            <li className='text-gray-900 text-base font-medium' key={index}>
+              <Link to={link.path}>{link.name}</Link>
+            </li>
+          ))}
+        </ul>
+        <Link to='/signup' className='bg-black hover:bg-white text-white hover:text-black px-3 py-1 rounded-lg border-2 border-black'>
+          <h1 className='text-base font-semibold'>Sign Up</h1>
+        </Link>
+      </div>
+    </div>
+  )
+}
