998988b4b85cd0ba0df81201c3bc3d97ea437a68
DanielAglr <155811921+dnlaglr@users.noreply.github.com>
2024-10-02 15:02:49 -0700

    Add more links to navbar

--- tiles/src/components/Navbar.tsx
+++ tiles/src/components/Navbar.tsx
@@ -3,8 +3,16 @@ import { Link } from 'react-router-dom';
 const navLinks = [
   {
     path: '/map',
-    name: 'Map'
+    name: 'Map',
   },
+  {
+    path: '/activity',
+    name: 'Activity',
+  },
+  {
+    path: '/friends',
+    name: 'Friends',
+  }
 ]
 
 export default function Navbar() {
@@ -14,16 +22,21 @@ export default function Navbar() {
         <Link to='/'>
           <h1 className='text-gray-900 text-2xl font-semibold'>Tiles</h1>
         </Link>
-        <ul>
+        <ul className='flex flex-row justify-evenly items-center w-96 h-full'>
           {navLinks.map((link, index) => (
             <li className='text-gray-900 text-base font-medium' key={index}>
               <Link to={link.path}>{link.name}</Link>
             </li>
           ))}
         </ul>
-        <Link to='/signup' className='bg-black hover:bg-white text-white hover:text-black px-3 py-1 rounded-lg border-2 border-black'>
-          <h1 className='text-base font-semibold'>Sign Up</h1>
-        </Link>
+        <div className='flex flex-row justify-between items-center w-52'>
+          <Link to='/signup' className='flex justify-center items-center w-24 bg-black hover:bg-white text-white hover:text-black py-1 rounded-lg border-2 border-black'>
+            <h1 className='text-base font-semibold'>Sign Up</h1>
+          </Link>
+          <Link to='/login' className='flex justify-center items-center w-24 bg-black hover:bg-white text-white hover:text-black py-1 rounded-lg border-2 border-black'>
+            <h1 className='text-base font-semibold'>Log In</h1>
+          </Link>
+        </div>
       </div>
     </div>
   )
